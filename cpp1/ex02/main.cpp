c35fa12378c37f636979ed908f7f3f382eb8b19a
sku <alexandre.hamad@hotmail.fr>
2026-04-01 14:40:24 +0200

    ok

--- cpp1/ex02/main.cpp
+++ cpp1/ex02/main.cpp
@@ -10,7 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+
 int main()
 {
-    
+    std::string str = "HI THIS IS BRAIN";
+    std::string* stringPTR = &str;
+    std::string& stringREF = str;
+
+    std::cout << "Adresse str       : " << &str << std::endl;
+    std::cout << "Adresse stringPTR : " << stringPTR << std::endl;
+    std::cout << "Adresse stringREF : " << &stringREF << std::endl;
+
+    std::cout << "\nValeur str       : " << str << std::endl;
+    std::cout << "Valeur stringPTR : " << *stringPTR << std::endl;
+    std::cout << "Valeur stringREF : " << stringREF << std::endl;
+
+    return (0);
 }
