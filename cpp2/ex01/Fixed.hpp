3730ff074aadc8b8bcce9eaecc0044ce4e850350
sku <alexandre.hamad@hotmail.fr>
2026-04-06 13:55:27 +0200

    ok

---
+++ cpp2/ex01/Fixed.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alehamad <alehamad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/06 13:44:26 by alehamad          #+#    #+#             */
+/*   Updated: 2026/04/06 13:44:26 by alehamad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed 
+{
+    public:
+        Fixed(const int value);
+        Fixed(const float value);
+        Fixed(const Fixed& other);
+        Fixed& operator=(const Fixed& other);
+        ~Fixed();
+
+        int getRawBits(void) const;
+        void setRawBits(int const raw);
+        float toFloat(void) const;
+        int toInt(void) const;
+
+    private:
+        int value;
+        static const int fractionalBits = 8;
+    };
+
+#endif
